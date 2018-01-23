0b5021f539a5026c4a9268da96d825511395b961
Benedek Farkas <benedek.farkas@lombiq.com>
2018-01-23 23:03:10 +0100

    Adding ShowResultCount BooleanField to ProjectionPart
    --HG--
    branch : dev

---
+++ Constants/FieldNames.cs
@@ -0,0 +1,10 @@
+﻿namespace Lombiq.Projections.Constants
+{
+    public static class FieldNames
+    {
+        public static class ProjectionPart
+        {
+            public const string ShowResultCount = nameof(ShowResultCount);
+        }
+    }
+}
