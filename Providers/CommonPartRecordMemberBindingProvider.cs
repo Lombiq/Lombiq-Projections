e48f19d32e83e6c8399132a43067e1c3238e3cc5
Benedek Farkas <benedek.farkas@lombiq.com>
2019-04-08 16:25:03 +0200

    Adding CommonPart.OwnerId filter as an IChainableMemberBindingProvider implementation
    --HG--
    branch : orchard-upgrade-1.11

---
+++ Providers/CommonPartRecordMemberBindingProvider.cs
@@ -0,0 +1,30 @@
+﻿using Orchard.Core.Common.Models;
+using Orchard.Localization;
+using System.Collections.Generic;
+
+namespace Lombiq.Projections.Providers
+{
+    public class CommonPartRecordMemberBindingProvider : IChainableMemberBindingProvider
+    {
+        public Localizer T { get; set; }
+
+
+        public CommonPartRecordMemberBindingProvider()
+        {
+            T = NullLocalizer.Instance;
+        }
+
+
+        public IEnumerable<ChainableMemberBinding> GetChainableMemberBindings() =>
+            new[]
+            {
+                new ChainableMemberBinding
+                {
+                    ContentPartRecordType = typeof(CommonPartRecord),
+                    PropertyPath = $"{nameof(CommonPartRecord.OwnerId)}",
+                    DisplayName = T("Owner ID"),
+                    Description = T("The User ID of the content item's owner.")
+                }
+            };
+    }
+}
