d5ae56a1c4f595324e9decc0cc01dfd05263efaa
Benedek Farkas <benedek.farkas@lombiq.com>
2019-04-08 21:12:39 +0200

    TokenizedContentTypeFilter now uses TokenizedValueListFilterForm instead of its own
    --HG--
    branch : orchard-upgrade-1.11

--- Projections/Filters/TokenizedContentTypeFilter.cs
+++ Projections/Filters/TokenizedContentTypeFilter.cs
@@ -2,7 +2,6 @@
 using Orchard.Localization;
 using Orchard.Projections.Descriptors.Filter;
 using Orchard.Projections.Services;
-using System;
 using System.Linq;
 
 namespace Lombiq.Projections.Projections.Filters
@@ -22,26 +21,24 @@ namespace Lombiq.Projections.Projections.Filters
         {
             describe.For("Content", T("Content"), T("Content"))
                 .Element("TokenizedContentTypes", T("Tokenized Content Types"), T("Tokenized Content Types"),
-                    ApplyFilter, DisplayFilter, TokenizedContentTypeFilterForm.FormName);
+                    ApplyFilter, DisplayFilter, TokenizedValueListFilterForm.FormName);
 
         }
 
         public void ApplyFilter(FilterContext context)
         {
-            var values = new TokenizedContentTypeFilterFormElements(context.State);
+            var formValues = new TokenizedValueListFilterFormElements(context.State);
 
-            if (!string.IsNullOrEmpty(values.ContentTypes))
-                context.Query = context.Query.ForType(values.ContentTypes
-                    .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(type => type.Trim()).ToArray());
+            if (formValues.Values.Any()) context.Query = context.Query.ForType(formValues.Values);
         }
 
         public LocalizedString DisplayFilter(FilterContext context)
         {
-            var values = new TokenizedContentTypeFilterFormElements(context.State);
+            var formValues = new TokenizedValueListFilterFormElements(context.State);
 
-            return string.IsNullOrEmpty(values.ContentTypes) ?
-                T("Any type of content.") :
-                T("Content with the type matching with \"{0}\".", values.ContentTypes);
+            return formValues.Values.Any() ?
+                T("Content with its type matching \"{0}\".", formValues.Values) :
+                T("Any type of content.");
         }
     }
 }
